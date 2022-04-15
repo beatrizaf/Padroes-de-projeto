240bf6456c450786aff1ebf61c2010b29ffcd9a3
Beatriz Araújo <beatrizbjj1@gmail.com>
2022-04-15 01:23:06 -0300

    Create JanelaMac.java

---
+++ Padrão Bridge/JanelaMac.java
@@ -0,0 +1,14 @@
+package br.padroes.bridge;
+
+public class JanelaMac implements  JanelaImplementada {
+
+    @Override
+    public void desenharJanela(String titulo){
+        System.out.println(titulo + " - Janela Mac");
+    }
+
+    @Override
+    public void desenharBotao(String titulo){
+        System.out.println(titulo + " - Botao Mac");
+    }
+}
